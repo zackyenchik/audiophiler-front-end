9115e90098bb9bdaa572104604da3a9ebd68ce5c
Zack <zyenchik@gmail.com>
2019-03-30 14:53:35 -0400

    Search page now keeps track of the query parameter.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,8 +3,16 @@ import NavBar from '../components/NavBar.jsx';
 import styles from '../styles.css'
 import {Container} from 'reactstrap';
 import CSHPlug from '../components/CSHPlug.jsx';
+import queryString from 'query-string';
 
 class Search extends React.Component {
+
+    componentDidMount() {
+        const params = queryString.parse(this.props.location.search);
+        console.log(params.q);
+        // TODO: make API call to search db and create/store audio file components for results
+    }
+
     render() {
         return(
             <div>
